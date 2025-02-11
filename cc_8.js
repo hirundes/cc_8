1162adb80367aabb2592659d7e33205dced4f849
HIRUN <hirundesilva97@gmail.com>
2025-02-11 15:08:58 -0500

    CHANGE: Task 1: added .toFixed(2) to match expected output

--- cc_8.js
+++ cc_8.js
@@ -1,8 +1,10 @@
 //Task 1 - Employee Salary Calculation 
 function calculateSalary(baseSalary, bonus, taxRate) {    //function calculateSalary
     let netSalary = ((baseSalary + bonus) - (baseSalary * taxRate));   //formula netSalary
-    console.log(`Net Salary: $${netSalary}`);   //logging netSalary for function
+    console.log(`Net Salary: $${netSalary.toFixed(2)}`);   //logging netSalary for function
 };
 console.log("---Task 1---");
 calculateSalary(5000, 500, 0.1); // Expected output: "Net Salary: $5000.00"
 calculateSalary(7000, 1000, 0.15); // Expected output: "Net Salary: $6950.00"
+
+
