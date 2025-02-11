dff9bff444be84f3d9dfbdf6c8f942c6859f0329
HIRUN <hirundesilva97@gmail.com>
2025-02-11 15:02:33 -0500

    Task 1 - Employee Salary Calculation

--- cc_8.js
+++ cc_8.js
@@ -1,1 +1,10 @@
-//Task 1 
+//Task 1 - Employee Salary Calculation 
+function calculateSalary(baseSalary, bonus, taxRate) {    //function calculateInvoice
+    let netSalary = ((baseSalary + bonus) - (baseSalary * taxRate));   //formula total
+    console.log(`Net Salary: $${netSalary}`);   //logging total invoice for function
+};
+console.log("---Task 1---");
+calculateSalary(5000, 500, 0.1); // Expected output: "Net Salary: $5000.00"
+calculateSalary(7000, 1000, 0.15); // Expected output: "Net Salary: $6950.00"
+
+
