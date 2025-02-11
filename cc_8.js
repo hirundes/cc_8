e1b2055c220edec3bff9c09b3a06faf93551e885
HIRUN <hirundesilva97@gmail.com>
2025-02-11 15:04:32 -0500

    CHANGE: Task 1 - added comments

--- cc_8.js
+++ cc_8.js
@@ -1,10 +1,8 @@
 //Task 1 - Employee Salary Calculation 
-function calculateSalary(baseSalary, bonus, taxRate) {    //function calculateInvoice
-    let netSalary = ((baseSalary + bonus) - (baseSalary * taxRate));   //formula total
-    console.log(`Net Salary: $${netSalary}`);   //logging total invoice for function
+function calculateSalary(baseSalary, bonus, taxRate) {    //function calculateSalary
+    let netSalary = ((baseSalary + bonus) - (baseSalary * taxRate));   //formula netSalary
+    console.log(`Net Salary: $${netSalary}`);   //logging netSalary for function
 };
 console.log("---Task 1---");
 calculateSalary(5000, 500, 0.1); // Expected output: "Net Salary: $5000.00"
 calculateSalary(7000, 1000, 0.15); // Expected output: "Net Salary: $6950.00"
-
-
