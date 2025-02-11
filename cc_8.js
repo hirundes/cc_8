0734875167cfcda717a1448e29ee33af48734b47
HIRUN <hirundesilva97@gmail.com>
2025-02-11 15:10:23 -0500

    Task 2 - Product Price After Discount

--- cc_8.js
+++ cc_8.js
@@ -8,3 +8,13 @@ calculateSalary(5000, 500, 0.1); // Expected output: "Net Salary: $5000.00"
 calculateSalary(7000, 1000, 0.15); // Expected output: "Net Salary: $6950.00"
 
 
+//Task 2 - Product Price After Discount
+const calculateDiscount = function(price, discountRate) {    // function expression calculateDiscount
+    let finalPrice = price - (price * discountRate);     //formula finalPrice
+    console.log(`Final Price: $${finalPrice.toFixed(2)}`);   //logging finalPrice w/ .toFixed(2)
+};
+console.log("---Task 2---");
+calculateDiscount(100, 0.2); // Expected output: "Final Price: $80.00"
+calculateDiscount(250, 0.15); // Expected output: "Final Price: $212.50"
+
+
